98c8ba87611128fca29bf1cb572980cb358d08fa
walk8243 <walk8243@gmail.com>
2017-12-15 00:35:03 +0900

    http接続確認

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('main js');
+const http  = require('http');
+
+const server = http.createServer();
+server.on('request', doRequest);
+server.listen(1234);
+console.log('Server running!');
+
+function doRequest(request, response){
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Site Open!');
+  response.end();
+}
