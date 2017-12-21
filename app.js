2fc8efad7690cf0494b10575ef39623b0b700eec
walk8243 <walk8243@gmail.com>
2017-12-22 01:06:37 +0900

    1階層に限りルーティング完了

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const http  = require('http'),
       fs		= require('fs'),
-      url		= require('url'),
-      yaml  = require('js-yaml');
+      url		= require('url');
 
 var export_function = require('./route.js');
-export_function.func1();
-export_function.func2('hoge');
-var test_func = export_function.func3();
 
 const server = http.createServer();
 server.on('request', doRequest);
@@ -19,11 +15,23 @@ function doRequest(request, response){
   if(url_parts.pathname == '/favicon.ico'){
     return;
   }
-  console.log(url_parts);
+  // console.log(url_parts);
 
   var url_result = export_function.func4(url_parts.pathname);
+  if(url_result === false){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('param Error!');
+    response.end();
+    return;
+  }else if(typeof url_result === 'object'){
+    var url_title = url_result[0];
+    var url_data = url_result[1];
+  }else{
+    var url_title = url_result;
+    var url_data = {};
+  }
 
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Site Open!' + test_func + url_result);
+  response.write('Site Open!' + url_title);
   response.end();
 }
--- route.js
+++ route.js
@@ -1,3 +1,14 @@
+const fs		= require('fs'),
+      yaml  = require('js-yaml');
+
+var route;
+try{
+  route = yaml.safeLoad(fs.readFileSync('./route.yml', 'utf8'));
+  // console.log(route);
+}catch(e){
+  console.log(e);
+}
+
 exports.func1 = function(){
 	console.log('function 1');
 }
@@ -11,9 +22,44 @@ exports.func3 = function(){
 }
 
 exports.func4 = function(pathname){
-  if(pathname.match(/\/\d+/i)){
-    return 'OK';
-  }else{
-    return 'NO';
+  // if(pathname.match(/\/\d+/i)){
+  //   return 'OK';
+  // }else{
+  //   return 'NO';
+  // }
+  // console.log(route);
+
+  if(pathname == "/"){
+    return route[""].title;
   }
+
+  var pathArray = pathname.split('/');
+  pathArray.shift();
+  console.log(pathArray);
+  var i = 0, returnData = [];
+  for(var key in route){
+    if(key == pathArray[i]){
+      returnData[0] = route[key].title;
+      returnData[1] = "";
+      return returnData;
+    }else if(key == "_num_"){
+      if(pathArray[i].match(/\d+/i)){
+        returnData[0] = route[key].title;
+        data = {};
+        data[route[key].var] = pathArray[i];
+        returnData[1] = data;
+        return returnData;
+      }
+    }else if(key == "_string_"){
+      if(pathArray[i].match(/\w+/i)){
+        returnData[0] = route[key].title;
+        data = {};
+        data[route[key].var] = pathArray[i];
+        returnData[1] = data;
+        return returnData;
+      }
+    }
+  }
+
+  return false;
 }
