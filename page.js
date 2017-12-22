bd82ffb788284457446d3eee5e2203b375331698
walk8243 <walk8243@gmail.com>
2017-12-22 17:40:32 +0900

    page.jsでtemplateフォルダ内のテンプレートファイルを全て読み込む

--- page.js
+++ page.js
@@ -1,8 +1,31 @@
+const fs		= require('fs');
+
+var templateFiles = {};
+loadTemplate('./template', templateFiles);
+// console.log(templateFiles);
+
 exports.pages = function(page, data){
   if(typeof eval(page) === 'function'){
-    index(data);
+    eval(page + "(data)"); // `route.yml`で指定している関数にジャンプ
   }else{
     // console.log("No!");
+  }
+}
+
+function loadTemplate(dirPath, tmpObj){
+  if(fs.existsSync(dirPath) && fs.statSync(dirPath).isDirectory()){
+    var files = fs.readdirSync(dirPath);
+    for(var i in files){
+      filePath = dirPath + "/" + files[i];
+      // console.log(filePath);
+      if(fs.statSync(filePath).isDirectory()){
+        tmpObj[files[i]] = {};
+        loadTemplate(filePath, tmpObj[files[i]]);
+      }else if(fs.statSync(filePath).isFile()){
+        tmp = files[i].split('.');
+        tmpObj[tmp[0]] = fs.readFileSync(filePath, 'utf8');
+      }
+    }
   }
 }
 
