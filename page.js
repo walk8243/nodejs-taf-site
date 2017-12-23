ce4b05934863b9436d297911b0127abcabc60eca
walk8243 <walk8243@gmail.com>
2017-12-23 17:06:32 +0900

    templateとしてはejsのみを読み込むように変更

--- page.js
+++ page.js
@@ -1,8 +1,11 @@
 const fs		= require('fs');
 
+// テンプレートファイルの読み込み
 var templateFiles = {};
 loadTemplate('./template', templateFiles);
 // console.log(templateFiles);
+
+
 
 exports.pages = function(page, data){
   if(typeof eval(page) === 'function'){
@@ -12,18 +15,23 @@ exports.pages = function(page, data){
   }
 }
 
+// `template`フォルダに存在する全ての`ejs`ファイルを読み込む
 function loadTemplate(dirPath, tmpObj){
   if(fs.existsSync(dirPath) && fs.statSync(dirPath).isDirectory()){
     var files = fs.readdirSync(dirPath);
     for(var i in files){
       filePath = dirPath + "/" + files[i];
+      file = fs.statSync(filePath);
       // console.log(filePath);
-      if(fs.statSync(filePath).isDirectory()){
+      if(file.isDirectory()){
         tmpObj[files[i]] = {};
         loadTemplate(filePath, tmpObj[files[i]]);
-      }else if(fs.statSync(filePath).isFile()){
+      }else if(file.isFile()){
         tmp = files[i].split('.');
-        tmpObj[tmp[0]] = fs.readFileSync(filePath, 'utf8');
+        if(tmp[1] == 'ejs'){
+          console.log(filePath);
+          tmpObj[tmp[0]] = fs.readFileSync(filePath, 'utf8');
+        }
       }
     }
   }
