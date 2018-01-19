e0cb949fad0720d212b42bc4f75f2ce82ebba588
walk8243 <walk8243@gmail.com>
2018-01-19 17:36:49 +0900

    DBの初期セットアッププログラム

--- install/db.js
+++ install/db.js
@@ -16,15 +16,70 @@ mysqlConnection.connect(function(error){
   console.log('Connected!');
 });
 
-fs.readFile('install/initial-db.sql', 'utf-8', function(error, sql){
+mysqlConnection.query({
+  sql: 'SHOW TABLES'
+}, function(error, results, fields){
   if(error){throw error;}
-  // console.log(sql);
+  // console.log(results);
+  // console.log(results.length);
 
-  mysqlConnection.query(sql, function(error, results, fields){
+  if(results.length == 0){
+    fs.readFile('install/initial-table.sql', 'utf-8', function(error, sql){
+      if(error){throw error;}
+      // console.log(sql);
+
+      mysqlConnection.query(sql, function(error, results, fields){
+        if(error){throw error;}
+        // console.log(results);
+
+        console.error('Table is Completed!');
+        // mysqlConnection.end();
+      });
+    });
+
+    fs.readFile('install/initial-data.sql', 'utf-8', function(error, sql){
+      if(error){throw error;}
+      // console.log(sql);
+
+      mysqlConnection.query(sql, function(error, results, fields){
+        if(error){throw error;}
+        // console.log(results);
+
+        console.log('Data is Completed!');
+        // mysqlConnection.end();
+      });
+    });
+  }else{
+    var errorMessage = `
+全てのテーブルを削除し、データベースを初期化した上で、実行して下さい。
+`;
+    console.log(errorMessage);
+  }
+
+  /*
+  // console.log(fields[0].name);
+  var table_name = fields[0].name;
+  var queryString = '';
+  for (var i in results) {
+    if (results.hasOwnProperty(i)) {
+      // console.log(results[i][table_name]);
+      queryString += results[i][table_name] + ', '
+    }
+  }
+
+  queryString = 'DROP TABLE ' + queryString.slice(0,-2) + ';';
+  console.log(queryString);
+
+  mysqlConnection.query({
+    sql: queryString
+  }, function(error, results, fields){
     if(error){throw error;}
-    // console.log(results);
 
-    console.log('Completed!');
-    mysqlConnection.end();
+    console.log(results);
   });
+  */
+  // mysqlConnection.end();
 });
+
+/*
+*/
