4067b8fcaf0eefb961a06c5bb78893a2a67e6bd6
walk8243 <walk8243@gmail.com>
2018-03-13 00:48:42 +0900

    複数の試合データから登録されている年度のみをリンクの表示

--- page/main/competition/index.js
+++ page/main/competition/index.js
@@ -4,6 +4,44 @@ class Index extends Page {
   constructor(){
     super();
   }
+
+  render(res, data){
+    // console.log(data);
+    this.pageData.param = data;
+
+    var htmlStr   = this.htmlStr,
+        pageData  = this.pageData;
+    var sql = `
+SELECT \`years\` AS 'year', COUNT(*) AS 'count'
+  FROM (
+    SELECT CASE
+      WHEN MONTH(\`start\`)>=4 THEN YEAR(\`start\`)
+      WHEN MONTH(\`start\`)<=3 THEN YEAR(\`start\`)-1
+      END AS 'years'
+      FROM \`competition\`
+  ) AS \`com-year\`
+  GROUP BY \`years\`
+  ORDER BY \`years\` DESC
+`;
+    mysqlConnection.query(
+      {
+        sql: sql,
+      },
+      function(error, results, fields){
+        if(error){throw error;}
+        console.log(results);
+        var comYear = [];
+        for(var result of results){
+          if(result.count > 0){
+            comYear.push(result.year);
+          }
+        }
+        pageData.years = comYear;
+        myFunc.renderEjs(res, htmlStr, pageData);
+      }
+    );
+
+  }
 }
 
 module.exports = new Index();
