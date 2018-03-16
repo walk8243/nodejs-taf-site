b455b484fe1b4b22b8108ef10c3af81c62cc3c1b
walk8243 <walk8243@gmail.com>
2018-03-16 17:06:34 +0900

    コンパイルする範囲の指定がファイルでもフォルダでも大丈夫なように変更

--- sass.js
+++ sass.js
@@ -5,12 +5,12 @@ class Sass {
     this.sassList = [];
   }
 
-  compile(dirPath = './lib'){
+  compile(path = './lib'){
     console.log('sass compile!');
     this.sassList = []; // sassファイルリストの初期化
 
-    this.searchSass(dirPath);
-    // console.log(this.sassList);
+    this.searchSass(path);
+    console.log(this.sassList);
     for(var sassPath of this.sassList){
       var result = sass.renderSync({
         file: sassPath.sass,
@@ -21,30 +21,39 @@ class Sass {
     }
   }
 
-  searchSass(dirPath){
-    if(myFunc.isExistFile(dirPath)
-      && fs.statSync(dirPath).isDirectory()){
-      var files = fs.readdirSync(dirPath);
-      for(var i in files){
-        var filePath = dirPath + "/" + files[i];
-        // console.log(filePath);
-        var file = fs.statSync(filePath);
-        if(file.isDirectory()){
-          this.searchSass(filePath);
-        }else if(file.isFile()){
-          if(filePath.match(/^.+\.(sass|scss)$/)){
-            var outputFilePath = filePath.replace(/\.(sass|scss)$/, '.css');
-            var obj = {
-              sass  : filePath,
-              output: outputFilePath
-            };
-            this.sassList.push(obj);
+  searchSass(path){
+    if(myFunc.isExistFile(path)){
+      if(fs.statSync(path).isDirectory()){
+        var files = fs.readdirSync(path);
+        for(var i in files){
+          var filePath = path + "/" + files[i];
+          // console.log(filePath);
+          var file = fs.statSync(filePath);
+          if(file.isDirectory()){
+            this.searchSass(filePath);
+          }else if(file.isFile()){
+            if(filePath.match(/\.(sass|scss)$/)){
+              this.addCompileFile(filePath);
+            }
           }
+        }
+      }else if(fs.statSync(path).isFile()){
+        if(path.match(/\.(sass|scss)$/)){
+          this.addCompileFile(path);
         }
       }
     }else{
-      console.log(0, [dirPath]);
+      console.log(0, [path]);
     }
+  }
+
+  addCompileFile(filePath){
+    var outputFilePath = filePath.replace(/\.(sass|scss)$/, '.css');
+    var obj = {
+      sass  : filePath,
+      output: outputFilePath
+    };
+    this.sassList.push(obj);
   }
 }
 
