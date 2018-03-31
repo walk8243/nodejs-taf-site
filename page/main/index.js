af3b6c69fbc9466be3ec4abb708c229c24359ea1
walk8243 <walk8243@gmail.com>
2018-03-31 16:49:01 +0900

    要らない箇所の削除

--- page/main/index.js
+++ page/main/index.js
@@ -1,20 +1,9 @@
 const Page = require(process.cwd() + '/page.js');
-var yaml  = require('js-yaml');
 
 class Index extends Page {
   constructor(){
     super();
   }
-
-  // render(res, data){
-  //   this.pageData.param = data;
-  //
-  //   var sendData = yaml.safeLoad(fs.readFileSync(process.cwd() + '/data/sample.yml', 'utf8'));
-  //   // console.log(sendData);
-  //   this.pageData = Object.assign(this.pageData, sendData);
-  //
-  //   myFunc.renderEjs(res, this.pageStr, this.pageData);
-  // }
 }
 
 module.exports = new Index();
