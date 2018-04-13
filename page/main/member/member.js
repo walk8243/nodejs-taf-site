65cebb11454ee46e1d7249c11b88ac8ed3fc5466
walk8243 <walk8243@gmail.com>
2018-04-13 23:40:09 +0900

    部員個別ページの表示だけ

---
+++ page/main/member/member.js
@@ -0,0 +1,48 @@
+const Page = require(process.cwd() + '/page.js');
+
+class Member extends Page {
+  constructor(){
+    super();
+  }
+
+  outputPage(res, data){
+    var pageObj = this;
+    // console.log(data);
+    pageObj.pageData.param = data;
+
+    var sql = `
+SELECT
+  CONCAT(\`name1\`, ' ', \`name2\`) AS 'name',
+  CONCAT(\`phonetic1\`, ' ', \`phonetic2\`) AS 'phonetic',
+  \`sex\`,
+  \`year\`,
+  \`grade\`,
+  \`degree\`,
+  \`expert\`,
+  \`graduate\`,
+  \`position\`,
+  \`image\`
+  FROM \`member\`
+  WHERE \`del_flag\` = 0
+    AND \`id\`=${data['member_id']}
+`;
+    mysqlConnection.query(
+      {
+        sql: sql,
+      },
+      function(error, results, fields){
+        if(error){throw error;}
+        // console.log(results);
+        var member = [];
+        for(var result of results){
+          member.push(result);
+        }
+        pageObj.pageData.member = member;
+        myFunc.renderEjs(res, pageObj.render());
+      }
+    );
+
+  }
+}
+
+module.exports = new Member();
